f62e9deac2ba99522c2390dba7349629046c2ecb
wondafrash <meandaida@gmail.com>
2024-06-29 12:28:50 -0500

    update hello-world code

---
+++ jwt-token/src/test/java/com/spring/security/jwt_token/JwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.jwt_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
